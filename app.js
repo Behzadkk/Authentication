9c92d1e2d887f2cda116a68d947ec93bb58744f6
behzadkk <bhzdkkvnd@gmail.com>
2019-08-31 18:08:56 +0100

    login and logout

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -32,7 +33,7 @@ app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/secret", function(req, res) {
+app.get("/secret", isLoggedIn, function(req, res) {
   res.render("secret");
 });
 
@@ -56,6 +57,32 @@ app.post("/register", function(req, res) {
     }
   );
 });
+
+// Login Routes
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+  }),
+  function(req, res) {}
+);
+
+// Logout Route
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 app.listen(port, function() {
   console.log("SERVER STARTED.....");
